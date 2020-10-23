919f25131de35fbdae278c9a841f8315dc009ed9
ElifSena-cal <66389332+ElifSena-cal@users.noreply.github.com>
2020-10-23 17:13:50 +0300

    Add files via upload

---
+++ AdonetProje/AdonetProje/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace AdonetProje
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.stocktxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.unittxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nametxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.unitUpdaate = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.StockUpdate = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nameUpdate = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Yellow;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(578, 252);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.Text = "dataGridView1";
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.stocktxt);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.unittxt);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.nametxt);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 286);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 218);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add Product";
+            // 
+            // stocktxt
+            // 
+            this.stocktxt.Location = new System.Drawing.Point(122, 109);
+            this.stocktxt.Name = "stocktxt";
+            this.stocktxt.Size = new System.Drawing.Size(115, 23);
+            this.stocktxt.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Stock Amount";
+            // 
+            // unittxt
+            // 
+            this.unittxt.Location = new System.Drawing.Point(122, 67);
+            this.unittxt.Name = "unittxt";
+            this.unittxt.Size = new System.Drawing.Size(115, 23);
+            this.unittxt.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Unit Price";
+            // 
+            // nametxt
+            // 
+            this.nametxt.Location = new System.Drawing.Point(122, 27);
+            this.nametxt.Name = "nametxt";
+            this.nametxt.Size = new System.Drawing.Size(115, 23);
+            this.nametxt.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Added";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // unitUpdaate
+            // 
+            this.unitUpdaate.Location = new System.Drawing.Point(122, 109);
+            this.unitUpdaate.Name = "unitUpdaate";
+            this.unitUpdaate.Size = new System.Drawing.Size(115, 23);
+            this.unitUpdaate.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Stock Amount";
+            // 
+            // StockUpdate
+            // 
+            this.StockUpdate.Location = new System.Drawing.Point(122, 67);
+            this.StockUpdate.Name = "StockUpdate";
+            this.StockUpdate.Size = new System.Drawing.Size(115, 23);
+            this.StockUpdate.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 117);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 15);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Unit Price";
+            // 
+            // nameUpdate
+            // 
+            this.nameUpdate.Location = new System.Drawing.Point(122, 27);
+            this.nameUpdate.Name = "nameUpdate";
+            this.nameUpdate.Size = new System.Drawing.Size(115, 23);
+            this.nameUpdate.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(55, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Updated";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 15);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.unitUpdaate);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.StockUpdate);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.nameUpdate);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(330, 286);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(287, 218);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Update Product";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(600, 76);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 25);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Deleted";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 516);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox stocktxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox unittxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nametxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox unitUpdaate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox StockUpdate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox nameUpdate;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ AdonetProje/AdonetProje/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdonetProje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ProductDal productDal = new ProductDal();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            GettAll();
+        }
+
+        private void GettAll()
+        {
+            dataGridView1.DataSource = productDal.GetAll();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            productDal.Added(new Product
+            {
+                Name = nametxt.Text,
+                UnitPrice = Convert.ToDecimal(unittxt.Text),
+                StockAmount = Convert.ToInt32(stocktxt.Text),
+
+
+            });
+            GettAll();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            nameUpdate.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            unitUpdaate.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            StockUpdate.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            productDal.Update(new Product
+            {
+                Id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value),
+                Name = nameUpdate.Text,
+                UnitPrice = Convert.ToDecimal(unitUpdaate.Text),
+                StockAmount = Convert.ToInt32(StockUpdate.Text)
+            });
+            GettAll();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            productDal.Deleted(id);
+            GettAll();
+        }
+    }
+}
